d2c5a3e64f34bf7790ae33dc61adb34198fe5962
vivsridh4 <vsridhar@digitalocean.com>
2018-12-02 11:17:26 +0530

    new code for testing

--- DO-community-example/activity-finder/handler.py
+++ DO-community-example/activity-finder/handler.py
@@ -5,7 +5,9 @@ def handle(req):
     r = requests.get("http://api.open-notify.org/astros.json")
     result = r.json()
     
-    index = random.randint(0, len(result["people"])-1)
-    name = result["people"][index]["name"]
+    index = random.randint(0, len(result["activities"])-1)
+    name = result["activities"][index]["name"]
+    desc = result["activities"][index]["desc"]
+    url = result["activities"][index]["URL"]
 
-    return "%s is in space" % (name)
+    return "%s for startups \\n %s \\n \\n Know more @ %s" % (name) %desc %url
